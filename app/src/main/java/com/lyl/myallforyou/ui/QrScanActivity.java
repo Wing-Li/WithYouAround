7e944b1f22ed9d4b6c9a2622a4e603076cfae3ed
lyl <iamlyl@foxmail.com>
2017-04-11 17:35:06 +0800

    二维码扫描

---
+++ app/src/main/java/com/lyl/myallforyou/ui/QrScanActivity.java
@@ -0,0 +1,17 @@
+package com.lyl.myallforyou.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lyl.myallforyou.R;
+
+public class QrScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_scan);
+    }
+
+
+}
---
+++ app/src/main/java/com/lyl/myallforyou/ui/QrShareActivity.java
@@ -0,0 +1,70 @@
+package com.lyl.myallforyou.ui;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+import com.lyl.myallforyou.R;
+import com.lyl.myallforyou.utils.AppUtils;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class QrShareActivity extends AppCompatActivity {
+
+    @Bind(R.id.qr_share_img)
+    ImageView qrShareImg;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_share);
+        ButterKnife.bind(this);
+
+        Bitmap bitmap = encodeAsBitmap(AppUtils.getUUID(QrShareActivity.this));
+        qrShareImg.setImageBitmap(bitmap);
+    }
+
+
+    private Bitmap encodeAsBitmap(String str) {
+        Bitmap bitmap = null;
+        BitMatrix result = null;
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            result = multiFormatWriter.encode(str, BarcodeFormat.QR_CODE, 450, 450);
+            // 使用 ZXing Android Embedded 要写的代码
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            bitmap = barcodeEncoder.createBitmap(result);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException iae) { // ?
+            return null;
+        }
+
+        // 如果不使用 ZXing Android Embedded 的话，要写的代码
+
+//        int w = result.getWidth();
+//        int h = result.getHeight();
+//        int[] pixels = new int[w * h];
+//        for (int y = 0; y < h; y++) {
+//            int offset = y * w;
+//            for (int x = 0; x < w; x++) {
+//                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+//            }
+//        }
+//        bitmap = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888);
+//        bitmap.setPixels(pixels,0,100,0,0,w,h);
+
+        return bitmap;
+    }
+}
