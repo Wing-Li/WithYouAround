24b486189d710a1863adcf8969f47db946d28186
lyl <iamlyl@foxmail.com>
2017-05-09 17:53:51 +0800

    静态代码简单修改

---
+++ app/src/main/java/com/lyl/myallforyou/ui/essay/NhEassayActivity.java
@@ -0,0 +1,15 @@
+package com.lyl.myallforyou.ui.essay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lyl.myallforyou.R;
+
+public class NhEassayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nh_eassay);
+    }
+}
